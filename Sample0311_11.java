2d5b248bbd78c5563be6f2d6960a1399ce4c15ba
Leeseunghyun520 <dj20250615@dge.go.kr>
2026-03-19 14:49:32 +0900

    Add files via upload

---
+++ Sample0311_11.java
@@ -0,0 +1,9 @@
+package jump2java;
+
+public class Sample0311_11 {
+    public static void main(String[] args) {
+        String num = "123";
+        int n = Integer.parseInt(num);
+        System.out.println(n);  // 123 출력
+    }
+}
