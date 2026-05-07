4abc5cfab6ad82225e76b453010c90efe8d4d2b9
Leeseunghyun520 <dj20250615@dge.go.kr>
2026-05-07 15:44:36 +0900

    Add files via upload

---
+++ TriangleLB.java
@@ -0,0 +1,20 @@
+package jump2java;
+import java.util.Scanner;
+public class TriangleLB {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("높이: ");
+		int num = sc.nextInt();
+		
+		for(int i =1; i <= num ; i++) {
+			for(int j = 1; j <= i ; j++) {
+				System.out.println("*");
+			}
+			System.out.println("");
+		}
+        sc.close();
+        
+	}
+
+}
---
+++ TriangleLU.java
@@ -0,0 +1,17 @@
+package jump2java;
+import java.util.Scanner;
+public class TriangleLU {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("높이 ");
+		int num = sc.nextInt();
+		
+		for(int i = 1; i <= num ; i--) {
+			for(int j = 1; i <= num ; j--) {
+				System.out.print("*");
+			}
+			System.out.println("\n");
+		}
+	}
+}
